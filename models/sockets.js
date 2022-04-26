d8a154163428ace3f464de91ca91fd4bc6788e0b
Nandier Ruiz <nandiersruiz@outlook.com>
2022-04-25 19:43:27 -0500

    band-names V1.0.0

---
+++ models/sockets.js
@@ -0,0 +1,53 @@
+const BandList = require("./band-list");
+
+
+class Sockets {
+
+    constructor(io) {
+
+        this.io = io;
+
+        this.bandList = new BandList();
+
+        this.socketEvents();
+    }
+
+    socketEvents() {
+        // On connection
+        this.io.on('connection', (socket) => {
+            console.log('cliente conectado');
+
+            // emitir al cliente conectado todas las bandas actuales
+            socket.emit('current-bands', this.bandList.getBands());
+
+            // votar por la banda
+            socket.on('votar-banda', (id) => {
+                this.bandList.increaseVotes(id);
+                this.io.emit('current-bands', this.bandList.getBands());
+            })
+
+            // eliminar banda
+            socket.on('eliminar-banda', (id) => {
+                this.bandList.removeBand(id);
+                this.io.emit('current-bands', this.bandList.getBands());
+            })
+
+            // cambiar nombre de banda
+            socket.on('cambiar-nombre-banda', ({id, nombre}) => {
+                this.bandList.changeBandName(id, nombre);
+                this.io.emit('current-bands', this.bandList.getBands());
+            })
+
+            // agregar banda
+            socket.on('crear-banda', ({ nombre }) => {
+                this.bandList.addBand(nombre);
+                this.io.emit('current-bands', this.bandList.getBands());
+            })
+        });
+    }
+
+
+}
+
+
+module.exports = Sockets;
